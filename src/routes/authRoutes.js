f04de3cbd1cede083ca35fe4d205b8890c350542
99lourduxavier <45066102+99lourduxavier@users.noreply.github.com>
2023-11-03 19:40:06 +0530

    Create authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// User egistration endpoint
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: 'User Registered Successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// User login endpoint
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ username }, 'secret-key', { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
